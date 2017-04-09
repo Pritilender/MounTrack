7034b8d2c38fde2730c2a87cee355f845a7f8045
Pritilender <sajn_ap@live.com>
2017-04-09 15:25:51 +0200

    feat(place): enrich place detail page

--- src/pages/place/place.ts
+++ src/pages/place/place.ts
@@ -2,12 +2,6 @@ import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {PlaceTypeLong} from '../places/places';
 
-/**
- * Generated class for the Place page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
     selector: 'page-place',
