95b5f3c7519fdbbcb92c26b7c5ca89fdc8048443
mladen994 <mladen994@gmail.com>
2017-04-12 20:45:36 +0200

    feat(places): add place removal

--- src/pages/places/places.ts
+++ src/pages/places/places.ts
@@ -1,5 +1,5 @@
 import {AfterViewInit, Component, OnDestroy} from "@angular/core";
-import {IonicPage, ModalController, NavController, NavParams} from "ionic-angular";
+import {AlertController, IonicPage, ModalController, NavController, NavParams} from "ionic-angular";
 import {EditPlace} from "../edit-place/edit-place";
 import {PlaceService, PlaceTypeShort} from "../../providers/place-service";
 import {NewPlace} from "../new-place/new-place";
@@ -30,16 +30,42 @@ export class Places {
     public newPlace() {
         let modal = this._modalCtrl.create(NewPlace);
 
+        modal.onDidDismiss(_ => this.update());
         modal.present();
     }
 
     constructor(public navCtrl: NavController,
                 public navParams: NavParams,
                 private _modalCtrl: ModalController,
-                private _placeService: PlaceService) {
+                private _placeService: PlaceService,
+                private _alertCtrl: AlertController) {
     }
 
     ionViewWillEnter() {
+        this.update();
+    }
+
+    public deletePlace(place: PlaceTypeShort): void {
+        let prompt = this._alertCtrl.create({
+            title: "Warning!",
+            message: `Are you sure you want to delete place "${place.name}" from your places list?`,
+            buttons: [
+                {
+                    text: 'No',
+                },
+                {
+                    text: 'Yes',
+                    handler: () => {
+                        this._placeService.deletePlace(place.id);
+                        this.update();
+                    }
+                }
+            ]
+        });
+        prompt.present();
+    }
+
+    private update() {
         this.places = this._placeService.getShortPlaces();
     }
 }
