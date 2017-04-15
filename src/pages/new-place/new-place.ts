c773e7b42f6c80eca94fe417de411c74427074a2
Pritilender <sajn_ap@live.com>
2017-04-15 14:44:02 +0200

    feat(location): add location tracking on new place addition

--- src/pages/new-place/new-place.ts
+++ src/pages/new-place/new-place.ts
@@ -1,13 +1,14 @@
-import {Component} from "@angular/core";
-import {AlertController, IonicPage, ViewController} from "ionic-angular";
+import {AfterViewInit, Component} from "@angular/core";
+import {AlertController, IonicPage, LoadingController, ViewController} from "ionic-angular";
 import {PlaceService, PlaceTypeLong} from "../../providers/place-service";
+import {Geolocation} from "@ionic-native/geolocation";
 
 @IonicPage()
 @Component({
     selector: 'page-new-place',
     templateUrl: 'new-place.html',
 })
-export class NewPlace {
+export class NewPlace implements AfterViewInit {
     public place: PlaceTypeLong = {
         id: '',
         name: '',
@@ -18,14 +19,40 @@ export class NewPlace {
         },
     };
 
+    private _geolocation = new Geolocation();
+
     constructor(private _viewCtrl: ViewController,
                 private _placeService: PlaceService,
-                private _alertCtrl: AlertController,) {
+                private _alertCtrl: AlertController,
+                private _loadingCtrl: LoadingController) {
+        let loading = this._loadingCtrl.create({
+            content: 'Fetching current position...',
+        });
+        loading.present();
+        // options are maybe not needed for real devices, only for emulators
+        this._geolocation.getCurrentPosition({
+            maximumAge: 0,
+            timeout: 5000,
+            enableHighAccuracy: true,
+        })
+            .then(position => {
+                this.place.coordinates.lat = position.coords.latitude;
+                this.place.coordinates.lng = position.coords.longitude;
+                loading.dismiss();
+            })
+            .catch((error: PositionError) => {
+                loading.dismiss();
+                alert(`Position error: '${error.message}'. Error code: ${error.code}`);
+            });
     }
+
+    public ngAfterViewInit(): void {
+    }
+
 
     public save(): void {
         if (this.place.name != '') {
-            let id = this._placeService.addNew(this.place);
+            this._placeService.addNew(this.place);
             this._viewCtrl.dismiss();
         } else {
             this._alertCtrl.create({
